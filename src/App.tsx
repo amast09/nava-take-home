fd2a2cd47c6176bebf7a9f8d1f9dfca69a5b94d9
Aaron Mast <amast09@gmail.com>
2022-11-30 21:42:00 -0500

    Add list of company benefits cards to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import AsyncRequest, {
+  asyncRequestCompleted,
+  AsyncRequestKinds,
+  asyncRequestLoading,
+  asyncRequestNotStarted,
+  voidAsyncRequestFailed,
+} from "./types/AsyncRequest";
+import { CompanyBenefits } from "./types/CompanyBenefits";
+import { CompanyBenefitsCard } from "./CompanyBenefitsCard";
+import { getCompanyData } from "./getCompanyData";
 
-function App() {
-  return <div>Palomar Light</div>;
-}
+type AllBenefitsProps = Readonly<{
+  companiesRequest: AsyncRequest<ReadonlyArray<CompanyBenefits>>;
+}>;
+
+const AllBenefits: React.FC<AllBenefitsProps> = ({ companiesRequest }) => {
+  switch (companiesRequest.kind) {
+    case AsyncRequestKinds.NotStarted:
+      return <></>;
+    case AsyncRequestKinds.Loading:
+      return <>Loading Company's Benefits</>;
+    case AsyncRequestKinds.Failed:
+      return <>Failed to Return Company's Benefits</>;
+    case AsyncRequestKinds.Completed:
+      return (
+        <>
+          {companiesRequest.result.map((company) => (
+            <CompanyBenefitsCard key={company.ein} company={company} />
+          ))}
+        </>
+      );
+  }
+};
+
+const App: React.FC = () => {
+  const [companiesRequest, setCompaniesRequest] = useState<
+    AsyncRequest<ReadonlyArray<CompanyBenefits>>
+  >(asyncRequestNotStarted());
+
+  useEffect(() => {
+    setCompaniesRequest(asyncRequestLoading());
+    getCompanyData()
+      .then((companiesBenefits) =>
+        setCompaniesRequest(asyncRequestCompleted(companiesBenefits)),
+      )
+      .catch(() => setCompaniesRequest(voidAsyncRequestFailed()));
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="benefit-cards-container">
+        <AllBenefits companiesRequest={companiesRequest} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
